c34a622474c0464b905f3e8300ef53abeeb421ef
Chang <ainurlindale@gmail.com>
2019-05-16 00:00:50 +0800

    basic done

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ class List extends React.Component {
   constructor(){
     super()
     this.changeHandler = this.changeHandler.bind( this );
+    this.submitHandler = this.submitHandler.bind( this );
   }
 
   state = {
-    list : [],
+    list : ["hahahah", "hehehehehe", "hohohohoho"],
     word : ""
   }
 
@@ -14,14 +15,28 @@ class List extends React.Component {
     console.log("change", event.target.value);
   }
 
+    submitHandler(event) {
+        console.log("word", this.state.word);
+        console.log("list", this.state.list);
+        let updatedList = this.state.list;
+        updatedList.push(this.state.word);
+        console.log(updatedList)
+        this.setState({list: updatedList});
+    }
+
   render() {
       // render the list with a map() here
+
+      let listItems = this.state.list.map( (item) => {
+        return <p> {item} </p>
+      })
 
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <input onChange={this.changeHandler}  value={this.state.word}/>
+          <button onClick={this.submitHandler}>add item</button>
+          <p> {listItems} </p>
         </div>
       );
   }
@@ -31,4 +46,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
